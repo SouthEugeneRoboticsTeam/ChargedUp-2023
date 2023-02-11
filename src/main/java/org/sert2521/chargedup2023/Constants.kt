404034bbf89b08d087818105f924ce1f15ee81ba
kturner44 <kturner44@4j.lane.edu>
2023-02-11 11:46:42 -0800

    copy over drivetrain

--- src/main/java/org/sert2521/chargedup2023/Constants.kt
+++ src/main/java/org/sert2521/chargedup2023/Constants.kt
@@ -1,10 +1,64 @@
 package org.sert2521.chargedup2023
 
+import edu.wpi.first.math.MatBuilder
+import edu.wpi.first.math.Matrix
+import edu.wpi.first.math.Nat
+import edu.wpi.first.math.geometry.*
+import edu.wpi.first.math.numbers.N1
+import edu.wpi.first.math.numbers.N3
+import org.sert2521.chargedup2023.PhysicalConstants.halfSideLength
+import kotlin.math.PI
+
+class SwerveModuleData(val position: Translation2d, val powerMotorID: Int, val angleMotorID: Int, val angleEncoderID: Int, val angleOffset: Double, val inverted: Boolean)
+
 object PhysicalConstants {
+    const val halfSideLength = 0.0
+
+    const val powerEncoderMultiplierPosition = 0.0
+    const val powerEncoderMultiplierVelocity = 0.0
+
+    const val angleEncoderMultiplier = 0.0
+
+    val tagPose = Pose3d(0.0, 0.0, 0.0, Rotation3d(0.0, 0.0, 0.0))
+    val cameraTrans = Transform3d(Translation3d(0.0, 0.0, 0.0), Rotation3d(0.0, 0.0, 0.0))
 }
 
 object TunedConstants {
+    const val swervePowerS = 0.0
+    const val swervePowerV = 0.0
+    const val swervePowerA = 0.0
+
+    const val swervePowerP = 0.0
+    const val swervePowerI = 0.0
+    const val swervePowerD = 0.0
+
+    const val swerveAngleP = 0.0
+    const val swerveAngleI = 0.0
+    const val swerveAngleD = 0.0
+
+    val stateDeviations: Matrix<N3, N1> = MatBuilder(Nat.N3(), Nat.N1()).fill(0.0, 0.0, 0.0)
+    val globalDeviations: Matrix<N3, N1> = MatBuilder(Nat.N3(), Nat.N1()).fill(0.0, 0.0, 0.0)
+}
+
+object ConfigConstants {
+    const val drivetrainOptimized = true
+
+    const val powerDeadband = 0.1
+    const val rotDeadband = 0.1
+    const val joystickDeadband = 0.1
+
+    const val driveSpeed = 1.0
+    const val rotSpeed = 1.0
+
+    const val joystickChangeSpeed = 0.4
 }
 
 object ElectronicIDs {
+    val swerveModuleData = mutableListOf(
+        SwerveModuleData(Translation2d(halfSideLength, -halfSideLength), 5, 2, 17, 5.36 - PI, false),
+        SwerveModuleData(Translation2d(-halfSideLength, -halfSideLength), 6, 10, 16, 0.29, false),
+        SwerveModuleData(Translation2d(halfSideLength, halfSideLength), 7, 1, 15, 4.82, true),
+        SwerveModuleData(Translation2d(-halfSideLength, halfSideLength), 8, 11, 14, 4.86, true))
+
+    const val camName = ""
 }
---
+++ src/main/java/org/sert2521/chargedup2023/subsystems/Drivetrain.kt
@@ -0,0 +1,290 @@
+package org.sert2521.chargedup2023.subsystems
+
+import com.ctre.phoenix.sensors.CANCoder
+import com.kauailabs.navx.frc.AHRS
+import com.revrobotics.CANSparkMax
+import com.revrobotics.CANSparkMaxLowLevel
+import edu.wpi.first.math.controller.PIDController
+import edu.wpi.first.math.controller.SimpleMotorFeedforward
+import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator
+import edu.wpi.first.math.geometry.*
+import edu.wpi.first.math.kinematics.*
+import edu.wpi.first.wpilibj.MotorSafety
+import edu.wpi.first.wpilibj2.command.SubsystemBase
+import org.photonvision.PhotonCamera
+import org.photonvision.targeting.PhotonPipelineResult
+import org.sert2521.chargedup2023.*
+import kotlin.math.PI
+import kotlin.math.atan2
+import kotlin.math.pow
+
+class SwerveModule(private val powerMotor: CANSparkMax,
+                   private val powerFeedforward: SimpleMotorFeedforward,
+                   private val powerPID: PIDController,
+                   private val angleMotor: CANSparkMax,
+                   private val angleEncoder: CANCoder,
+                   private val angleOffset: Double,
+                   private val anglePID: PIDController,
+                   private val centerRotation: Rotation2d,
+                   private val inverted: Boolean,
+                   var state: SwerveModuleState,
+                   shouldOptimize: Boolean,
+                   brakeMode: Boolean) : MotorSafety() {
+    var doesOptimize = shouldOptimize
+        private set
+
+    var position: SwerveModulePosition
+
+    init {
+        if (doesOptimize) {
+            anglePID.enableContinuousInput(-PI, PI)
+        } else {
+            anglePID.enableContinuousInput(-PI * 2, PI * 2)
+        }
+
+        setMotorMode(brakeMode)
+
+        position = SwerveModulePosition(getDistance(), getAngle())
+
+        powerMotor.inverted = inverted
+
+        powerMotor.encoder.positionConversionFactor = PhysicalConstants.powerEncoderMultiplierPosition
+        powerMotor.encoder.velocityConversionFactor = PhysicalConstants.powerEncoderMultiplierVelocity
+    }
+
+    private fun getDistance(): Double {
+        return powerMotor.encoder.position
+    }
+
+    private fun getVelocity(): Double {
+        return powerMotor.encoder.velocity
+    }
+
+    private fun getAngle(): Rotation2d {
+        return if (inverted) {
+            Rotation2d(-(angleEncoder.absolutePosition * PhysicalConstants.angleEncoderMultiplier - angleOffset))
+        } else {
+            Rotation2d(angleEncoder.absolutePosition * PhysicalConstants.angleEncoderMultiplier - angleOffset)
+        }
+    }
+
+    fun setOptimize(value: Boolean) {
+        doesOptimize = value
+
+        if (doesOptimize) {
+            anglePID.enableContinuousInput(-PI, PI)
+        } else {
+            anglePID.enableContinuousInput(-PI * 2, PI * 2)
+        }
+    }
+
+    // Should be called in periodic
+    fun updateState() {
+        val angle = getAngle()
+        state = SwerveModuleState(getVelocity(), angle)
+        position = SwerveModulePosition(getDistance(), angle)
+    }
+
+    fun set(wanted: SwerveModuleState) {
+        // Using state because it should be updated and getVelocity and getAngle (probably) spend time over CAN
+        val optimized = if (doesOptimize) {
+            SwerveModuleState.optimize(wanted, state.angle)
+        } else {
+            wanted
+        }
+
+        val feedforward = powerFeedforward.calculate(optimized.speedMetersPerSecond)
+        val pid = powerPID.calculate(state.speedMetersPerSecond, optimized.speedMetersPerSecond)
+        // Figure out voltage stuff
+        // Could multiply by cos angle
+        // Why isn't motor.inverted working
+        if (!inverted) {
+            powerMotor.set((feedforward + pid) / 12.0)
+        } else {
+            powerMotor.set(-(feedforward + pid) / 12.0)
+        }
+        angleMotor.set(anglePID.calculate(state.angle.radians, optimized.angle.radians))
+    }
+
+    fun enterBrakePos() {
+        set(SwerveModuleState(0.0, centerRotation))
+    }
+
+    fun setMotorMode(coast: Boolean) {
+        if (coast) {
+            powerMotor.idleMode = CANSparkMax.IdleMode.kCoast
+            angleMotor.idleMode = CANSparkMax.IdleMode.kCoast
+        } else {
+            powerMotor.idleMode = CANSparkMax.IdleMode.kBrake
+            angleMotor.idleMode = CANSparkMax.IdleMode.kBrake
+        }
+    }
+
+    override fun stopMotor() {
+        powerMotor.stopMotor()
+        angleMotor.stopMotor()
+    }
+
+    override fun getDescription(): String {
+        return "Swerve Module"
+    }
+}
+
+object Drivetrain : SubsystemBase() {
+    private val imu = AHRS()
+
+    private val cam = PhotonCamera(ElectronicIDs.camName)
+    private var prevRes: PhotonPipelineResult? = null
+
+    private val kinematics: SwerveDriveKinematics
+    private var modules: Array<SwerveModule>
+    private val odometry: SwerveDriveOdometry
+    private val poseEstimator: SwerveDrivePoseEstimator
+
+    var pose = Pose2d()
+
+    var odometryPose = Pose2d()
+        private set
+        get() = odometry.poseMeters
+
+    // False is broken
+    var doesOptimize = ConfigConstants.drivetrainOptimized
+        private set
+
+    init {
+        val modulePositions = mutableListOf<Translation2d>()
+        val modulesList = mutableListOf<SwerveModule>()
+
+        for (moduleData in ElectronicIDs.swerveModuleData) {
+            val powerMotor = CANSparkMax(moduleData.powerMotorID, CANSparkMaxLowLevel.MotorType.kBrushless)
+            val angleMotor = CANSparkMax(moduleData.angleMotorID, CANSparkMaxLowLevel.MotorType.kBrushless)
+
+            modulePositions.add(moduleData.position)
+            modulesList.add(createModule(powerMotor, angleMotor, moduleData))
+        }
+
+        modules = modulesList.toTypedArray()
+
+        val positions = mutableListOf<SwerveModulePosition>()
+
+        for (module in modules) {
+            module.updateState()
+            positions.add(module.position)
+        }
+
+        val positionsArray = positions.toTypedArray()
+
+        kinematics = SwerveDriveKinematics(*modulePositions.toTypedArray())
+        poseEstimator = SwerveDrivePoseEstimator(kinematics, imu.rotation2d, positionsArray, Pose2d(), TunedConstants.stateDeviations, TunedConstants.globalDeviations)
+        odometry = SwerveDriveOdometry(kinematics, imu.rotation2d, positionsArray)
+    }
+
+    private fun createModule(powerMotor: CANSparkMax, angleMotor: CANSparkMax, moduleData: SwerveModuleData): SwerveModule {
+        return SwerveModule(powerMotor,
+            SimpleMotorFeedforward(TunedConstants.swervePowerS, TunedConstants.swervePowerV, TunedConstants.swervePowerA),
+            PIDController(TunedConstants.swervePowerP, TunedConstants.swervePowerI, TunedConstants.swervePowerD),
+            angleMotor,
+            CANCoder(moduleData.angleEncoderID),
+            moduleData.angleOffset,
+            PIDController(TunedConstants.swerveAngleP, TunedConstants.swerveAngleI, TunedConstants.swerveAngleD),
+            Rotation2d(atan2(moduleData.position.y, moduleData.position.x)),
+            moduleData.inverted,
+            SwerveModuleState(),
+            doesOptimize,
+            true
+        )
+    }
+    override fun periodic() {
+        val res = cam.latestResult
+        if (res != prevRes) {
+            if (res.hasTargets()) {
+                val camToTargetTrans = res.bestTarget.bestCameraToTarget
+                val camPose = PhysicalConstants.tagPose.transformBy(camToTargetTrans.inverse())
+                poseEstimator.addVisionMeasurement(camPose.transformBy(PhysicalConstants.cameraTrans).toPose2d(), res.timestampSeconds)
+            }
+
+            prevRes = res
+        }
+
+        val positions = mutableListOf<SwerveModulePosition>()
+
+        for (module in modules) {
+            module.updateState()
+            val pos = module.position
+            pos.angle += Rotation2d(PI / 2)
+            positions.add(pos)
+        }
+
+        val positionsArray = positions.toTypedArray()
+
+        pose = poseEstimator.update(imu.rotation2d, positionsArray)
+        odometry.update(imu.rotation2d, positionsArray)
+    }
+
+    fun setOptimize(value: Boolean) {
+        doesOptimize = value
+
+        for (module in modules) {
+            module.setOptimize(doesOptimize)
+        }
+    }
+
+    fun setNewPose(newPose: Pose2d) {
+        pose = newPose
+
+        val positions = mutableListOf<SwerveModulePosition>()
+
+        for (module in modules) {
+            module.updateState()
+            positions.add(module.position)
+        }
+
+        val positionsArray = positions.toTypedArray()
+
+        odometry.resetPosition(imu.rotation2d, positionsArray, pose)
+        poseEstimator.resetPosition(imu.rotation2d, positionsArray, pose)
+    }
+
+    fun getAccelSqr(): Double {
+        return (imu.worldLinearAccelY.pow(2) + imu.worldLinearAccelX.pow(2)).toDouble()
+    }
+
+    private fun feed() {
+        for (module in modules) {
+            module.feed()
+        }
+    }
+
+    fun drive(chassisSpeeds: ChassisSpeeds) {
+        // Maybe desaturate wheel speeds
+        val wantedStates = kinematics.toSwerveModuleStates(chassisSpeeds)
+
+        for (i in wantedStates.indices) {
+            modules[i].set(wantedStates[i])
+        }
+
+        feed()
+    }
+
+    fun enterBrakePos() {
+        for (module in modules) {
+            module.enterBrakePos()
+        }
+
+        feed()
+    }
+
+    fun setMode(coast: Boolean) {
+        for (module in modules) {
+            module.setMotorMode(coast)
+        }
+    }
+
+    fun stop() {
+        for (module in modules) {
+            module.stopMotor()
+        }
+
+        feed()
+    }
+}
