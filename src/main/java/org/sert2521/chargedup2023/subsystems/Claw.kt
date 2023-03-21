7b88c6e3b88c4076913e41274784f8e667cd9fcf
fruzzmuffin <benji.urbancic@gmail.com>
2023-03-20 18:01:57 -0700

    Fixed autos

--- src/main/java/org/sert2521/chargedup2023/subsystems/Claw.kt
+++ src/main/java/org/sert2521/chargedup2023/subsystems/Claw.kt
@@ -9,26 +9,35 @@ import org.sert2521.chargedup2023.ElectronicIDs
 
 object Claw : SubsystemBase() {
     private val motor = CANSparkMax(ElectronicIDs.clawMotorId, CANSparkMaxLowLevel.MotorType.kBrushless)
+    private var currentSpeed = 0.0
 
     init {
         motor.idleMode = CANSparkMax.IdleMode.kBrake
     }
 
     override fun periodic() {
+        if (currentSpeed == 0.0){
+            motor.setSmartCurrentLimit(20)
+            motor.set(0.1)
+        }
+
         // Put in constants
         if (RobotController.getBatteryVoltage() <= ConfigConstants.armBrownOutVoltage) {
             motor.setSmartCurrentLimit(20)
         } else {
             motor.setSmartCurrentLimit(45)
         }
+
     }
 
     fun setMotor(speed:Double){
         motor.set(speed)
+        currentSpeed = speed
     }
 
     fun stop(){
         motor.stopMotor()
+        currentSpeed = 0.0
     }
 
 }
